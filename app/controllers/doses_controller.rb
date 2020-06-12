0354fe1cebb8b38ddd8b797ced43cd3b1bc76721
Louis Bemberg <louis.bemberg@gmail.com>
2020-06-12 11:14:31 +0200

    Finished the basic features of the app, cocktails & doses. frontend next

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,17 +9,23 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @newdose = Dose.new(dose_params)
     @newdose.cocktail_id = @cocktail.id
-    @newdose.save
-    redirect_to cocktail_path(cocktails_path)
+    if @newdose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 
-Ingredient.destroy_all
 puts "1 - Cleaning Database"
+Ingredient.destroy_all
 
+puts "2. Opening JSON and parsing it into a ruby hash"
+json_string = open(url).read
+ruby_hash = JSON.parse(json_string)
+
+puts "3.Creating ingredients"
+final_array = []
+ ruby_hash["drinks"].each do |ingredient_baby_hash|
+  final_array << Ingredient.create!(name: ingredient_baby_hash["strIngredient1"] )
+end
+puts "Successfully seeded ingredients in the database. :)"
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
@@ -21,4 +33,4 @@ Cocktail.create(name: "Bloody Mary")
 Cocktail.create(name: "Gin Tonic")
 Cocktail.create(name: "James Bond's Martini")
 
-puts "Successfully seeded ingredients in the database. :)"
+puts "5. You have #{Ingredient.count} ingredients seeded "
